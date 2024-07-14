c25592aa8a6bd977d004acd38eac52ffe9dcff49
Justin Nolan <justinnolan@Justins-MacBook-Pro.local>
2024-07-13 20:16:03 -0700

    updated

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "02d6d6494daf272b47068d0df56d851f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "9a29b1a4f7244ed1a5cd5cd53c6b08e3"
